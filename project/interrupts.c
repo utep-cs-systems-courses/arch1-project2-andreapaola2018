66aecd974ea715393d2e9dee48b71416d71efb3a
Andrea Ulloa <apulloa@miners.utep.edu>
2020-03-27 18:37:21 -0600

    added P2

--- project/interrupts.c
+++ project/interrupts.c
@@ -1,10 +1,11 @@
 #include <msp430.h>
 #include "stateMachines.h"
 
-/* Switch on P2 */
-__interrupt_vec(WD2_VECTOR) WDT(){
+
+void
+__interrupt_vec(WDT_VECTOR) WDT(){
   static char count = 0;
-  if(++blink_count == 125){
+  if(++count == 125){
     state_advance();
     count = 0; 
   }
