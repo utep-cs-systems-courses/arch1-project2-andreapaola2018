204cfecfdd5567cf63a6a2876cc03854968920a4
Andrea Ulloa <apulloa@miners.utep.edu>
2020-03-27 19:04:05 -0600

    update switches

--- project/switches.c
+++ project/switches.c
@@ -4,9 +4,9 @@
 #include "buzzer.h"
 #include "led.h"
 
-char switch_state_down, switch_state_changed;
+char state1, state2, state3,state4, switch_state_down, switch_state_changed;
 
-statich char switch_update_interrupt_sense()
+static char switch_update_interrupt_sense()
 {
   char p2val = P2IN;
   P2IES |= (p2val & SWITCHES);
@@ -17,55 +17,27 @@ statich char switch_update_interrupt_sense()
 void switch_init()
 
 {
-  P2REN |= switches;
+  P2REN |= SWITCHES;
   P2IE = SWITCHES;
   P2OUT |= SWITCHES;
   P2DIR &= ~SWITCHES;
   switch_update_interrupt_sense();
-  state_zero();
+  led_update();
 }
 
 void switch_interrupt_handler()
 {
-  char p2val = switch_update_interrupt_sense();
-  switch_state_changed = 1; // state has changed
 
-  /* 4 states, checking if running or not */ 
+  /* switches on the toy */
+  char p2val = switch_update_interrupt_sense();
+  
+
+  /* 4 states or switches, checking if running or not */ 
   state1 = (p2val & SW1) ? 0 : 1;
   state2 = (p2val & SW2) ? 0 : 1;
   state3 = (p2val & SW3) ? 0 : 1;
   state4 = (p2val & SW4) ? 0 : 1;
 
-  if(state1) {
-    switch_state_down = state1;
-    switch_state_changed = 1;
-    /* call to song sound */
-
-    
-  }
-  else if(state2) {
-    switch_state_down = state2;
-    switch_state_changed = 1;
-    /*   */
-  }
-  else if(state3) {
-    switch_state_down = state3;
-    switch_state_changed = 1; 
-  }
-
-  else if(state4) {
-    switch_state_down = state4;
-    switch_state_changed = 1; 
-  }
-
-  else{
-    switch_state_down = state4;
-    switch_state_changed = 1; 
-  }
-
-  else{
-    switch_state_down = 0;
-    switch_state_changed = 1; 
-
-  }
+  switch_state_changed = 1;
+  
 }
