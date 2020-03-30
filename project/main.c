1e868212d3f05dd34cbf879425028f06787571eb
Andrea Ulloa <apulloa@miners.utep.edu>
2020-03-29 20:45:47 -0600

    changed function definition

--- project/main.c
+++ project/main.c
@@ -1,15 +1,17 @@
 #include <msp430.h>
+#include "libTimer.h"
 #include "led.h"
 #include "switches.h"
 #include "buzzer.h"
-#include "libTimer.h"
 
-void main(void) {
-  configureClocks();
-  switch_init();/* initiating switches */
-  buzzer_init(); /* initiating buzzer  */ 
-  led_init(); 
+/* sets clocks, and interrupts */ 
+int main(void) {
+  configureClocks(); // setting CPU and clocks 
+  led_init();
   enableWDTInterrupts();
+  switch_init();/* initiating switches */
+  buzzer_init(); /* initiating buzzer  */  
+  
 
   or_sr(0x18);
 }
