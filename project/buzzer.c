b744c1f8c9a2c58f1bff5e96c445999bf301c32b
Andrea Ulloa <apulloa@miners.utep.edu>
2020-04-24 18:37:20 -0600

    update song

--- project/buzzer.c
+++ project/buzzer.c
@@ -15,18 +15,16 @@ void buzzer_init()
 void buzzer_set_period(short cycles) /* buzzer clock = 2MHz (period of 1k results in 2kHz tone) */
 {
   CCR0 = cycles;
-  CCR1 = cycles >> 1; /* one ahlf cycle */
+  CCR1 = cycles >> 1; /* one half cycle */
 }
 
 void sound(){
+  int song[15] = {523,523,784,784,440,440,784,698,698,659,659,587,587,523};
 
   int i = 0;
-  int sounds[] ={150, 1000, 900, 700, 805}; // different pitches of sound for buzzer
-
-  while(i < sizeof(sounds))
-  {
-    buzzer_set_period(sounds[i]);
-    state_advance();
-    i++;
+  while(i<16){
+    buzzer_set_period(song[i]); /* plays each note */
+    __delay_cycles(4000000); /* delays next note from playing */ 
   }
+  buzzer_set_period(0); /* sound ends */
 }
