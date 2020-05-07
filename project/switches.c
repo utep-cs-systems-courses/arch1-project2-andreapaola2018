45a7e4adb60da94453d470b88f4bce95ef8dff36
Andrea Ulloa <apulloa@miners.utep.edu>
2020-05-07 16:04:29 -0600

    added comment done

--- project/switches.c
+++ project/switches.c
@@ -4,24 +4,28 @@
 #include "buzzer.h"
 #include "led.h"
 
-char state1, state2, state3,state4, switch_state_down, switch_state_changed;
+char state1, state2, state3,state4,switch_state_changed;
 
 static char switch_update_interrupt_sense()
 {
   char p2val = P2IN;
-  P2IES |= (p2val & SWITCHES);
-  P2IES &= (p2val | ~SWITCHES);
+  /* update switch interrupt to detect changes from current buttons */
+  P2IES |= (p2val & SWITCHES); /* if switch up, sense down */
+  P2IES &= (p2val | ~SWITCHES); /* if down, sense up */
   return p2val;
 }
 
+
+/*setting up the switch */ 
 void switch_init()
 
 {
-  P2REN |= SWITCHES;
-  P2IE = SWITCHES;
-  P2OUT |= SWITCHES;
-  P2DIR &= ~SWITCHES;
+  P2REN |= SWITCHES; /* enables resistors for switches */
+  P2IE = SWITCHES; /* enable interrupts from switches */
+  P2OUT |= SWITCHES; /*pull-ups for switches */
+  P2DIR &= ~SWITCHES; /* set switches bits for input */
   switch_update_interrupt_sense();
+  
   led_update();
 }
 
@@ -33,11 +37,31 @@ void switch_interrupt_handler()
   
 
   /* 4 states or switches, checking if running or not */ 
-  state1 = (p2val & SW1) ? 0 : 1;
-  state2 = (p2val & SW2) ? 0 : 1;
-  state3 = (p2val & SW3) ? 0 : 1;
-  state4 = (p2val & SW4) ? 0 : 1;
-
-  switch_state_changed = 1;
+  state1 = (p2val & SW1) ? 0 : SW1;
+  state2 = (p2val & SW2) ? 0 : SW2;
+  state3 = (p2val & SW3) ? 0 : SW3;
+  state4 = (p2val & SW4) ? 0 : SW4;
   
+  /* play one song */
+  if(state1){
+    sound();
+  }
+  /* have green light flicker while red stays on */
+  if(state2){
+   
+    enableWDTInterrupts(); // turns on leds
+    
+  }
+  /*same as state 2 but buzzer sound */
+  if(state3){
+    enableWDTInterrupts();
+    buzzer_set_period(1000);
+    
+  }
+  
+  if(state4){
+    greenOn();
+    buzzer_set_period(800);
+  }
+    
 }
