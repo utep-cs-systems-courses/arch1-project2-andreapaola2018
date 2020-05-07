142a3638349cc54d96e6e95da36f133e20ce25dd
Andrea Ulloa <apulloa@miners.utep.edu>
2020-05-07 15:35:55 -0600

    added song

--- project/buzzer.c
+++ project/buzzer.c
@@ -9,11 +9,11 @@ void buzzer_init(){
   P2SEL2 &= ~(BIT6 | BIT7);
   P2SEL &= ~BIT7;
   P2SEL |= BIT6;
-  P2DIR = BIT6; /*enable output to speaker */
+  P2DIR = BIT6; /*enable output to speaker (P2.6) */
   
 }
 
-void buzzer_set_period(short cycles){
+void buzzer_set_period(short cycles){ /* buzzer clock = 2MHz. (period of 1k results in 2kHz tone)*/
 
   CCR0 = cycles;
   CCR1 = cycles >> 1;
